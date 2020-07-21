83cda39f69ce47de0982f679a712dcd848bda0b6
Olamide Ogunde <oogunde0413@gmail.com>
2020-07-21 15:35:57 -0700

    scaffolding back up

---
+++ AsyncInn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+using AsyncInn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace AsyncInn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+
+    }
+}
